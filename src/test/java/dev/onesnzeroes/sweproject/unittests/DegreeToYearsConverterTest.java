1be6cb749ef2fa39565b953d8d54c7b9f8e632c7
leevivl <leevi.laune@metropolia.fi>
2026-01-29 10:31:30 +0200

    refactor junit for degree to years

--- src/test/java/dev/onesnzeroes/sweproject/unittests/DegreeToYearsConverterTest.java
+++ src/test/java/dev/onesnzeroes/sweproject/unittests/DegreeToYearsConverterTest.java
@@ -5,16 +5,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DegreeToYearsConverterTest {
+    DegreeToYearsConverter degree = new DegreeToYearsConverter();
 
     @Test
-    public void degreeToYearsTest(){
-        DegreeToYearsConverter degree = new DegreeToYearsConverter();
-        assertEquals(4,degree.degreeToYears("Bsc"));
-        assertEquals(2,degree.degreeToYears("Msc"));
+    public void bscShouldBe4(){
+        assertEquals(4,degree.degreeToYears("BSc"));
+    }
+    @Test
+    public void mscShouldBe2(){
+        assertEquals(2,degree.degreeToYears("MSc"));
+    }
+    @Test
+    public void phdShouldBe5(){
         assertEquals(5,degree.degreeToYears("PHd"));
-
-        assertEquals(-1,degree.degreeToYears("hs"));
-
+    }
+    @Test
+    public void unsupportedShouldBeNegative(){
+        assertEquals(-1,degree.degreeToYears("HS"));
+    }
+    @Test
+    public void nullShouldThrowError(){
         assertThrows(NullPointerException.class, () -> degree.degreeToYears(null));
     }
 }
